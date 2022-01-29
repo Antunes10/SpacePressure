ffa7cb8f386041d1058b694efa38f955a63325c6
Paulo do Ó <paulojfdoo@gmail.com>
2022-01-29 12:29:31 +0000

    added interactable obejcts highlight

--- Assets/Scripts/Interactables/Interactable.cs
+++ Assets/Scripts/Interactables/Interactable.cs
@@ -2,7 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Outline))]
 public abstract class Interactable : MonoBehaviour
 {
+    private Outline outline;
     public abstract void Interact();
+
+    public void Highlight() {
+        SetOutlineAlpha(1);
+    }
+
+    public void EndHighlight() {
+        SetOutlineAlpha(0);
+    }
+
+    void Start() {
+        outline = GetComponent<Outline>();
+        SetOutlineAlpha(0);
+    }
+
+    void SetOutlineAlpha(float val) {
+        Color outlineColor = outline.OutlineColor;
+        outlineColor.a = val;
+        outline.OutlineColor = outlineColor;
+    }
 }
--- Assets/Scripts/Interactables/PlayerInteract.cs
+++ Assets/Scripts/Interactables/PlayerInteract.cs
@@ -6,16 +6,34 @@ public class PlayerInteract : MonoBehaviour
 {
     public float interactDistance;
     public LayerMask layerMask;
+
+    private Interactable highlightedObject;
     void Update() {
-        if(Input.GetKeyDown(KeyCode.Mouse0)) {
-            Transform camera = Camera.main.transform;
-            RaycastHit hit;
-            // Does the ray intersect any objects excluding the player layer
-            Debug.DrawRay(camera.position, camera.TransformDirection(Vector3.forward) * interactDistance, Color.yellow);
-            if (Physics.Raycast(camera.position, camera.TransformDirection(Vector3.forward), out hit, interactDistance, layerMask))
-            {
-                hit.collider.gameObject.GetComponent<Interactable>()?.Interact();
+        Transform camera = Camera.main.transform;
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        Debug.DrawRay(camera.position, camera.TransformDirection(Vector3.forward) * interactDistance, Color.yellow);
+        if (Physics.Raycast(camera.position, camera.TransformDirection(Vector3.forward), out hit, interactDistance, layerMask))
+        {
+            Interactable interactable = hit.collider.gameObject.GetComponent<Interactable>();
+            if(interactable != null) {
+                if(highlightedObject != null && interactable != highlightedObject) {
+                    highlightedObject.EndHighlight();
+                }
+
+                interactable.Highlight();
+                highlightedObject = interactable;
+
+                if(Input.GetKeyDown(KeyCode.Mouse0)) {
+                    highlightedObject.Interact();
+                }
             }
+
+            
+        } else if (highlightedObject != null) {
+            highlightedObject.EndHighlight();
+            highlightedObject = null;
         }
+        
     }
 }
